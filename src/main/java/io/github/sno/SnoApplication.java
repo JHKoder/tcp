4c817602270ad50de4aa4786e14f6f16a8134b07
kang <kang@KJH-github-oineh.local>
2022-10-30 16:09:58 +0900

    init

---
+++ src/main/java/io/github/sno/SnoApplication.java
@@ -0,0 +1,13 @@
+package io.github.sno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sno/SnoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
