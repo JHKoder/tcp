f4e0c0f4824f937ff6dd48f2ad19e16e127104cf
kang's <jeonghun.kang.dev@gmail.com>
2022-12-21 23:25:54 +0900

    :sparkles: host 추가

---
+++ src/main/java/io/github/sno/domain/network/Host.java
@@ -0,0 +1,41 @@
+package io.github.sno.domain.network;
+
+import java.util.Arrays;
+
+public class Host {
+    private final int classA;
+    private final int classB;
+    private final int classC;
+    private final int classD;
+
+    public Host(int classA, int classB, int classC, int classD) {
+        this.classA = classA;
+        this.classB = classB;
+        this.classC = classC;
+        this.classD = classD;
+        valid();
+    }
+
+    public static Host from(String ip) {
+        Integer[] arr = Arrays.stream(ip.split(".")).map(Integer::parseInt).toArray(Integer[]::new);
+
+        return new Host(arr[0], arr[1], arr[2], arr[3]);
+    }
+
+    private void valid() {
+        validateRange(classA);
+        validateRange(classB);
+        validateRange(classC);
+        validateRange(classD);
+    }
+
+    public String toString() {
+        return classA + "." + classB + "." + classC + "." + classD;
+    }
+
+    private void validateRange(int address) {
+        if (address > 255 || address < 0) {
+            throw new IllegalArgumentException("ip index 범위를 벗어 났습니다.");
+        }
+    }
+}
