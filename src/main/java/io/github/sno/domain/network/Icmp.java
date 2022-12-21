42b35b75212afa6455d206bae1f9b5270afed61d
kang's <jeonghun.kang.dev@gmail.com>
2022-12-21 23:25:46 +0900

    :sparkles: icmp 추가

---
+++ src/main/java/io/github/sno/domain/network/Icmp.java
@@ -0,0 +1,30 @@
+package io.github.sno.domain.network;
+
+import io.github.sno.monitor.NetStatus;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Icmp implements NetProtocal {
+    private final Host host;
+
+    public Icmp(Host host)  {
+        this.host = host;
+    }
+
+    public NetStatus connect() {
+        try {
+            return connect(InetAddress.getByName(host.toString()));
+        } catch (IOException e) {
+            return NetStatus.NOT_CONNECT;
+        }
+    }
+
+    private NetStatus connect(InetAddress inetAddress) throws IOException {
+        if (inetAddress.isReachable(10_000)) {
+            return NetStatus.OK;
+        } else {
+            return NetStatus.TIMEOUT;
+        }
+    }
+}
