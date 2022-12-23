e9622166b8e0ec16e16b3b2dbfaae95425c4a6ea
kang's <jeonghun.kang.dev@gmail.com>
2022-12-23 15:19:30 +0900

    :truck: 파일 위치 이동

--- src/test/java/io/github/sno/monitoring/NetWorkTest.java
+++ src/test/java/io/github/sno/monitoring/NetWorkTest.java
@@ -1,10 +1,10 @@
 package io.github.sno.monitoring;
 
-import io.github.sno.domain.network.Host;
-import io.github.sno.domain.network.Icmp;
-import io.github.sno.domain.network.NetWork;
-import io.github.sno.domain.network.Tcp;
-import io.github.sno.monitor.NetStatus;
+import io.github.sno.network.Host;
+import io.github.sno.network.NetStatus;
+import io.github.sno.network.NetWork;
+import io.github.sno.network.icmp.Icmp;
+import io.github.sno.network.tcp.Tcp;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,6 +31,19 @@ public class NetWorkTest {
 
         assertThat(status.name()).isEqualTo(NetStatus.OK.name());
     }
+
+//    @Test
+//    void TCP_Port_연결시_소켓_가져오기() {
+//        Host host = new Host(127, 0, 0, 1);
+//        Tcp tcp = new Tcp.Port(host, 5000);
+//        NetWork<Tcp> net = new NetWork<>(tcp);
+//
+//        NetStatus status = net.connect();
+//        Socket socket = tcp.getSocket();
+//
+//        assertThat(status.name()).isEqualTo(NetStatus.OK.name());
+//        assertThat(socket).isNotNull();
+//    }
 
     @Test
     void ICMP_연결_테스트() {
