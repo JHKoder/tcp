5033c15da78febd910d75b59af56d9131e7a8abe
oineh <jeonghun.kang.dev@gmail.com>
2023-03-07 14:41:32 +0900

    :recycle: NetTaskDator refactor

--- src/main/java/io/github/sno/network/monitor/tcpip/NetTaskDator.java
+++ src/main/java/io/github/sno/network/monitor/tcpip/NetTaskDator.java
@@ -4,16 +4,21 @@ import io.github.sno.network.monitor.tasks.Task;
 import io.github.sno.network.monitor.tasks.TaskResultData;
 import io.github.sno.network.monitor.tasks.TaskResultDator;
 
-public class NetTaskData implements TaskResultDator {
+public class NetTaskDator implements TaskResultDator {
     private final Task task;
     private final NetStatus data;
 
-    public NetTaskData(Task task, TaskResultData data) {
+    public NetTaskDator(Task task, TaskResultData data) {
         this.task =  task;
         this.data = (NetStatus) data;
     }
 
-    public static TaskResultDator of(Task task, TaskResultData data) {
-        return new NetTaskData(task,data);
+    public TaskResultDator of(Task task, TaskResultData data) {
+        return new NetTaskDator(task,data);
+    }
+
+    @Override
+    public boolean equalsTask(Task task) {
+        return this.task.equals(task);
     }
 }
